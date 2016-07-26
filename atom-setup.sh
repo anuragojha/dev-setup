d0ba7cbc35217cf09c698a8799984d66330ef2ce
Anurag Ojha <anuragojha@gmail.com>
2016-07-26 14:21:04 -0700

    Create atom-setup.sh

---
+++ atom-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# install atom first
+apm install atom-beautify
+apm install language-hql
+apm install git-time-machine
+apm install rst-preview-pandoc
+apm install language-restructuredtxt
