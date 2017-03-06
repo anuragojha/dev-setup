08850e0e175302b8a552d1a18a9ea0a44e38a39c
Anurag Ojha <anuragojha@gmail.com>
2017-03-05 20:13:58 -0800

    Update other-setup.sh

--- other-setup.sh
+++ other-setup.sh
@@ -1,4 +1,8 @@
-# pdf
+# mac config
+brew install dockutil
+dockutil --remove all   # cleanup doc bar on fresh install
 
+
+# pdf
 wget -O /usr/local/bin/cpdf http://github.com/coherentgraphics/cpdf-binaries/raw/master/OSX-Intel/cpdf
 chmod +x /usr/local/bin/cpdf
